2d727b8ecd042ba94593b4a70e9b72039711062c
pppest <ppest@protonmail.com>
2020-09-18 14:23:07 -0500

    added choice for icon

--- devault-ticker.10s.py
+++ devault-ticker.10s.py
@@ -5,12 +5,14 @@ import json
 #setings
 FIAT = ["mxn","usd","btc"]
 DECIMALS = 8
-WIKIPEDIA_ICON=urllib2.urlopen( "https://en.wikipedia.org/static/favicon/wikipedia.ico")
+ICON = True
 
 for f in FIAT:
     url2 = "https://api.coingecko.com/api/v3/simple/price?ids=devault&vs_currencies="+f
     result = urllib2.urlopen(url2).read()
     json_data = json.loads(result)
     coin_price = json_data['devault'][f]
-    print("DVT/"+f.upper()+" "+str(round(coin_price,DECIMALS))+" | iconName=devault-128")
-
+    if ICON:
+        print("DVT/"+f.upper()+" "+str(round(coin_price,DECIMALS))+" | iconName=devault-128")
+    else:
+        print("DVT/"+f.upper()+" "+str(round(coin_price,DECIMALS)))
